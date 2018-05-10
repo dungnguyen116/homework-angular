bc7634c286b60db981145167e913ded59f792863
dungnguyen116 <nguyentrungdung116@gmail.com>
2018-05-10 17:18:37 +0700

    update 10-5

--- Hw-TD/projectHomework/src/app/app.component.ts
+++ Hw-TD/projectHomework/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Http , Response} from '@angular/http'
+import * as $ from 'jquery'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,7 +10,7 @@ export class AppComponent {
   status: string;
   title = 'app';
   movies : Array<object>;
-
+  pages : number;
   constructor(private http : Http){ }
 
   ngOnInit() :void{
@@ -19,8 +20,24 @@ export class AppComponent {
   private async loadMovies(){
     this.status = 'Loading .........';
     const sub = this.http.get('https://yts.am/api/v2/list_movies.json').subscribe((response : Response)=>{
-      response.json().data.limit
       this.movies = response.json().data.movies;
+      let calPage = response.json().data.movie_count % response.json().data.limit;
+      if(calPage == 0){
+        this.pages = response.json().data.movie_count / response.json().data.limit;
+      }else{
+        this.pages = Math.ceil(response.json().data.movie_count / response.json().data.limit);
+      }
+      this.status = 'Load Done !'
+      sub.unsubscribe();
+    }, error =>{
+      console.log(error);
+      this.status = 'Load Error !';
+      sub.unsubscribe();
+    });
+  }
+  private async onChangePageClick(){
+    const sub = this.http.get('https://yts.am/api/v2/list_movies.json?page=').subscribe((response : Response)=>{
+
       this.status = 'Load Done !'
       sub.unsubscribe();
     }, error =>{
