234724f7304b319566be260969d0e57af8286264
dungnguyen116 <nguyentrungdung116@gmail.com>
2018-05-11 13:39:36 +0700

    update 11-5

--- Hw-TD/projectHomework/src/app/app.component.ts
+++ Hw-TD/projectHomework/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Http, Response } from '@angular/http'
 import * as $ from 'jquery'
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,12 +9,10 @@ import * as $ from 'jquery'
 })
 export class AppComponent {
   status: string;
-  title = 'app';
-  movies: Array<object>;
   pages: Array<number>;
   totalPages: number;
   p: number = 1;
-  constructor(private http: Http) { }
+  constructor(private http: Http,private router: Router) { }
 
   ngOnInit(): void {
     this.loadMovies();
@@ -24,7 +22,6 @@ export class AppComponent {
   private async loadMovies() {
     this.status = 'Loading .........';
     const sub = this.http.get('https://yts.am/api/v2/list_movies.json').subscribe((response: Response) => {
-      this.movies = response.json().data.movies;
       let calPage = response.json().data.movie_count % response.json().data.limit;
       if (calPage == 0) {
         calPage = response.json().data.movie_count / response.json().data.limit;
@@ -41,6 +38,8 @@ export class AppComponent {
       sub.unsubscribe();
     });
   }
- 
+  onItemClicked(id) {
+    this.router.navigate([`/browse-page/${id}`]);
+  }
   
 }
--- Hw-TD/projectHomework/src/app/app.routes.ts
+++ Hw-TD/projectHomework/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
-import {PagesComponent} from '../app/pages/pages.component'
+import { PagesComponent } from './pages/pages.component';
+import { AppComponent } from './app.component';
+
+
+
 export const routes: Routes = [
-    { path: '', redirectTo: '/', pathMatch: 'full' },
-    { path: 'browse-page/:page', component:PagesComponent}
+    { path: '', redirectTo: '/browse-page', pathMatch: 'full' },
+    {path: 'browse-page', component: AppComponent},
+    { path: 'browse-page/:id', component: PagesComponent }
+    
 ];
